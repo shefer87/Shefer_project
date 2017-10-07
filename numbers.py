6e6c4308962cc059992ab6a411e2a13a4ff4e116
shefer87 <31551992+shefer87@users.noreply.github.com>
2017-10-07 23:37:15 +0300

    Update numbers.py

--- numbers.py
+++ numbers.py
@@ -1,6 +1,6 @@
 numbers = []
 for number in range(1,1000):
-    if number%3 ==0 or if number%5 ==0:
+    if number%3 ==0 or number%5 ==0:
         numbers.append(number)
     else:
         continue
